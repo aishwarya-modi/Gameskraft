7fd2cff7c8c319283fc173a55df6414b719cadec
AISHWARYA <50246502+aishwarya-modi@users.noreply.github.com>
2024-04-06 15:11:18 +0530

    createUser

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -5,10 +5,9 @@ async function createUser(req, res) {
     const { latitude, longitude, userName, emailID } = req.body;
     if (!res) return res.status(400).json({ error: 'Request body missing' });
     const { userID, latitude, longitude, userName, emailID, password, favoriteGenre, preferredPlatform } = res;
+    if (!userID || !latitude || !longitude || !userName || !emailID || !password || !favoriteGenre || !preferredPlatform) return res.status(400).json({ error: 'Request body missing' });
 
     try {
-        const user = await User.create({ latitude, longitude, userName, emailID });
-        const user = await User.create({ userID, latitude, longitude, userName, emailID, password, favoriteGenre, preferredPlatform, connections: [], games: [], geohash_1: '', geohash_2: '', geohash_3: '', geohash_4: '', geohash_5: '' });
         res.json(user);
     } catch (error) {
         res.status(500).json({ error: 'Error while creating new user' });
@@ -22,9 +21,11 @@ async function createUser(req, res) {
 async function updateUser(req, res) {
     const { id } = req.params;
     const { latitude, longitude, userName, emailID } = req.body;
+    const { userID, latitude, longitude, userName, emailID, password, favoriteGenre, preferredPlatform } = req.body;
 
     try {
         const user = await User.findByIdAndUpdate(id, { latitude, longitude, userName, emailID }, { new: true });
+        const user = await User.findByIdAndUpdate(id, { userID, latitude, longitude, userName, emailID, password, favoriteGenre, preferredPlatform }, { new: true });
         res.json(user);
     } catch (error) {
         res.status(500).json({ error: 'Error while updating User' });
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const router = express.Router();
 const MatchController = require('../controllers/MatchController');
 const UserController = require('../controllers/UserController');
+const GameController = require('../controllers/GameController');
+const MessageController = require('../controllers/MessageController');
+const ChatController = require('../controllers/chatController');
+const Chat = require('../schemas/chatSchema');
 
 // routes for UserController
 router.post('/createUser', UserController.createUser);
@@ -11,4 +15,18 @@ router.delete('/deleteUser', UserController.deleteUser);
 
 // routes for MatchController
 router.get('/getMatches', MatchController.getMatches);
+
+//routes for GameController
+router.post('/createGame', GameController.createGame);
+
+// routes for MessageController
+router.post('/createMessage', MessageController.createMessage);
+
+// routes for ChatController
+router.post('/createChat', ChatController.createChat);
+router.get('/getChat', ChatController.getChat);
+
+
+
+
 module.exports = router;
